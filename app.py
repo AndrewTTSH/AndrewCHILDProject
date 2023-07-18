e135c3c2ff71dbc5b542b058fd24934e1b28a144
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-18 17:24:06 +0800

    Update app.py

--- app.py
+++ app.py
@@ -85,7 +85,7 @@ if __name__ == "__main__":
         st.session_state['total_cost'] = 0.0
 
     # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
-    st.sidebar.title("Sidebar")
+    st.sidebar.title("CHILD Project")
     model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
     counter_placeholder = st.sidebar.empty()
     counter_placeholder.write(
