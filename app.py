2168d9fbc3565cd60e2f4cc4d6a16fea9a1c0986
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-18 17:06:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -75,7 +75,34 @@ def main():
 if __name__ == "__main__":
     main()
 
-    
+    # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+    st.sidebar.title("Sidebar")
+    model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    counter_placeholder = st.sidebar.empty()
+    counter_placeholder.write(
+        f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+
+    # Map model names to OpenAI model IDs
+    if model_name == "GPT-3.5":
+        model = "gpt-3.5-turbo"
+    else:
+        model = "gpt-4"
+
+    # reset everything
+    if clear_button:
+        st.session_state['generated'] = []
+        st.session_state['past'] = []
+        st.session_state['messages'] = [
+            {"role": "system", "content": "You are a helpful assistant."}
+        ]
+        st.session_state['number_tokens'] = []
+        st.session_state['model_name'] = []
+        st.session_state['cost'] = []
+        st.session_state['total_cost'] = 0.0
+        st.session_state['total_tokens'] = []
+        counter_placeholder.write(
+            f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")    
 
 
 
