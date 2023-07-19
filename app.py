a7526393927ee8e56da478b2a2e4e9e22378fc47
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-19 10:17:48 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,48 +8,11 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 from dotenv import load_dotenv
-from langchain.chat_models import ChatOpenAI
-
-
-try:
-  # completion llm
-  llm = ChatOpenAI(
-      openai_api_key=OPENAI_API_KEY,
-      model_name='gpt-3.5-turbo',
-      temperature=0.0
-  )
-
-  qa_with_sources = RetrievalQAWithSourcesChain.from_chain_type(
-      llm=llm,
-      chain_type="stuff",
-      retriever=vectorstore.as_retriever()
-  )
-
-  result = qa_with_sources(query)
-  #print(result)
-  print('Question: {}'.format(result['question']))
-  print('Answer: {}'.format(result['answer']))
-  print('Sources: {}'.format(result['sources']))
-except Exception as e:
-  error_message = ''
-  # Just print(e) is cleaner and more likely what you want,
-  # but if you insist on printing message specifically whenever possible...
-  if hasattr(e, 'message'):
-      error_message = e.message
-  else:
-      error_message = e
-  print('ERROR MESSAGE: {}'.format(error_message))
-
 load_dotenv()
-
-
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-
 os.environ['OPENAI_API_KEY'] = OPENAI_API_KEY
-
-
 def doc_preprocessing():
     loader = DirectoryLoader(
         'data/',
@@ -63,7 +26,6 @@ def doc_preprocessing():
     )
     docs_split = text_splitter.split_documents(docs)
     return docs_split
-
 @st.cache_resource
 def embedding_db():
     # we use the openAI embedding model
@@ -79,10 +41,8 @@ def embedding_db():
         index_name='langchain-pdf'
     )
     return doc_db
-
 llm = ChatOpenAI()
 doc_db = embedding_db()
-
 def retrieval_answer(query):
     qa = RetrievalQA.from_chain_type(
     llm=llm, 
@@ -92,21 +52,40 @@ def retrieval_answer(query):
     query = query
     result = qa.run(query)
     return result
-
 def main():
-    [theme]
-    base="light"
     st.title("CHILD Projects")
-    st.image('./banner.jpg')
-
     text_input = st.text_input("Ask your query about any CHILD project. Code adapted from open source, built by Andrew Soh") 
     if st.button("Ask Query"):
         if len(text_input)>0:
             st.info("Your Query: " + text_input)
             answer = retrieval_answer(text_input)
             st.success(answer)
-            print('Sources: {}'.format(result['sources']))
+if __name__ == "__main__":
+    main()
+    if 'model_name' not in st.session_state:
+        st.session_state['model_name'] = []
+    if 'cost' not in st.session_state:
+        st.session_state['cost'] = []
+    if 'total_tokens' not in st.session_state:
+        st.session_state['total_tokens'] = []
+    if 'total_cost' not in st.session_state:
+        st.session_state['total_cost'] = 0.0
 
+
+    st.image('./banner.jpg')
+
+    # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
+    st.sidebar.title("CHILD Projects")
+    model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
+    counter_placeholder = st.sidebar.empty()
+    counter_placeholder.write(
+        f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
+    clear_button = st.sidebar.button("Clear Conversation", key="clear")
+    # Map model names to OpenAI model IDs
+    if model_name == "GPT-3.5":
+        model = "gpt-3.5-turbo"
+    else:
+        model = "gpt-4"
 
 
 
