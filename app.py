c0f1d766168586dcb0d4c135e3781bc2add73749
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-19 10:13:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from langchain.chat_models import ChatOpenAI
 import streamlit as st
 from dotenv import load_dotenv
 from langchain.chat_models import ChatOpenAI
-from langchain.chains import RetrievalQAWithSourcesChain
+
 
 try:
   # completion llm
@@ -107,21 +107,6 @@ def main():
             st.success(answer)
             print('Sources: {}'.format(result['sources']))
 
-
-
-    # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
-    st.sidebar.title("CHILD Projects")
-    model_name = st.sidebar.radio("Choose a model:", ("GPT-3.5", "GPT-4"))
-    counter_placeholder = st.sidebar.empty()
-    counter_placeholder.write(
-        f"Total cost of this conversation: ${st.session_state['total_cost']:.5f}")
-    clear_button = st.sidebar.button("Clear Conversation", key="clear")
-
-    # Map model names to OpenAI model IDs
-    if model_name == "GPT-3.5":
-        model = "gpt-3.5-turbo"
-    else:
-        model = "gpt-4"
 
 
 
