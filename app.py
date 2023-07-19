3cdd2b5be38c75c8ae6eadca3d187ddf7fcc2296
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-19 10:19:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -8,6 +8,7 @@ from langchain.chains import RetrievalQA
 from langchain.chat_models import ChatOpenAI
 import streamlit as st
 from dotenv import load_dotenv
+
 load_dotenv()
 PINECONE_API_KEY = os.getenv('PINECONE_API_KEY')
 PINECONE_ENV = os.getenv('PINECONE_ENV')
@@ -54,6 +55,8 @@ def retrieval_answer(query):
     return result
 def main():
     st.title("CHILD Projects")
+    st.image('./banner.jpg')
+    
     text_input = st.text_input("Ask your query about any CHILD project. Code adapted from open source, built by Andrew Soh") 
     if st.button("Ask Query"):
         if len(text_input)>0:
@@ -70,9 +73,6 @@ if __name__ == "__main__":
         st.session_state['total_tokens'] = []
     if 'total_cost' not in st.session_state:
         st.session_state['total_cost'] = 0.0
-
-
-    st.image('./banner.jpg')
 
     # Sidebar - let user choose model, show total cost of current conversation, and let user clear the current conversation
     st.sidebar.title("CHILD Projects")
