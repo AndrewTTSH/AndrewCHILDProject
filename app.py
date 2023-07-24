a33daf8980bc0705761c5fd1e4e222d71492ac87
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-24 15:57:25 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
                 unsafe_allow_html=True)
     st.image('./banner.jpg')
     st.text("Query on any topics related to healthcare and innovation")
-    st.markdown("[![banner]('./banner.jpg')](https://child.chi.sg)")
+    st.markdown("[![banner]('https://github.com/isomerpages/ttsh-chi-child/blob/staging/images/banner.jpg?raw=true')](https://child.chi.sg)")
 
 
 
