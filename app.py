e5c3e7ef3a9a309e9d80bec9609b24faa4e6a103
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-24 15:55:15 +0800

    Update app.py

--- app.py
+++ app.py
@@ -61,9 +61,10 @@ def retrieval_answer(query):
 def main():
     st.markdown("<h1 style='text-align: center;'>CHILD Projects </h1>",
                 unsafe_allow_html=True)
-    st.image("[banner]('./banner.jpg')(https://child.chi.sg)")
+    st.image('./banner.jpg')
     st.text("Query on any topics related to healthcare and innovation")
-    st.markdown("[![banner](/banner.jpg)](https://child.chi.sg)")
+    st.markdown("[![banner]('./banner.jpg')](https://child.chi.sg)")
+
 
 
     text_input = st.text_input("Ask your query about any CHILD project. Code adapted from open source, built by Andrew Soh") 
