985a63e241df8ada10ae31cd85c16569e9fe8171
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-24 16:06:49 +0800

    Update app.py

--- app.py
+++ app.py
@@ -90,8 +90,6 @@ if __name__ == "__main__":
         st.caption("Developed from open source codes for a search based on CHILD collection of projects.")
         st.caption("Do be aware that there will be longer waiting time as the AI search through our knowledge base")
         st.write("Check out our [CHILD website here](https://child.chi.sg)")
-
-
         st.write("[National Health Group](https://child.chi.sg)")
         st.write("[SingHealth](https://child.chi.sg)")
         st.write("[National University Health](https://child.chi.sg)")
