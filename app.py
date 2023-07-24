cd1040933a62ad21e86a7ad8522460a20970e19d
AndrewTTSH <132964618+AndrewTTSH@users.noreply.github.com>
2023-07-24 15:49:21 +0800

    Update app.py

--- app.py
+++ app.py
@@ -63,7 +63,7 @@ def main():
                 unsafe_allow_html=True)
     st.image('./banner.jpg')
     st.text("Query on any topics related to healthcare and innovation")
-    st.markdown("[![banner](/banner.jpg)](https://child.chi.sg)")
+    st.markdown("[![banner]('./banner.jpg')](https://child.chi.sg)")
     st.markdown("[![Foo](./banner.jpg)](https://child.chi.sg)")
 
 
